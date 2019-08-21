005d34a100409a6d9acba8803ef329693189ddf2
swapnilkamble3924 <50038839+swapnilkamble3924@users.noreply.github.com>
2019-08-21 21:25:12 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -42,6 +42,7 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        // added from github
     }
 
     @Override
